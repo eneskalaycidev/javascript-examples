23ab5ba5167dab6efb77c9b1f39ca4d68f25e625
eneskalaycidev <76259790+eneskalaycidev@users.noreply.github.com>
2021-01-18 21:35:23 +0300

    Add files via upload

---
+++ javascript examples 1.js
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>eneskalaycidev</title>
+    </head>
+    <body>
+    
+        
+<h2>First JavaScript Implementation</h2>
+ 
+<button type="button"
+onclick="document.getElementById('test').innerHTML = Date()">
+Print the date of the day</button>
+ 
+<p id="test"></p>
+    </body>
+</html>
